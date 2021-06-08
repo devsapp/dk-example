b23f512c024b2dc2b83d9c031c660a3794cf8093
huangfushan <wb-hfs624703@alibaba-inc.com>
2021-06-08 11:14:50 +0800

    feat: 🎸 add dk-tablestore-mail
    新增了 dk-tablestore-mail 用例

---
+++ dk-tablestore-mail/readme.md
@@ -0,0 +1,65 @@
+## 使用说明
+
+- 请前往 [tablestore 控制台](https://otsnext.console.aliyun.com/)创建实例， 在实例详情页面可以看到 `实例名称` 和 `公网地址`
+![image](https://img.alicdn.com/imgextra/i2/O1CN01VF6kv724mMdiMPC9q_!!6000000007433-2-tps-2184-1190.png)
+- 创建数据库表
+- 下载命令行工具：`npm install -g @serverless-devs/s`
+- 初始化一个模版项目：`s init devsapp/dk-tablestore-mail`，填写示例名称，公网地址，表名，发送邮箱，发送邮箱SMTP授权码，接收邮箱
+- 手动更改 baseURL
+- 部署函数：`s deploy`
+
+## TODO:优化
+
+- 在控制台 `自定义域名` 页面，更改路由设置，将路径 /index => /index/\*
+  ![image](https://img.alicdn.com/imgextra/i4/O1CN01s0xSAU1LCUucIA9OB_!!6000000001263-2-tps-1370-712.png)
+
+- 然后访问 `${域名}/index/`就可以访问了
+  ![image](https://img.alicdn.com/imgextra/i2/O1CN014D9vzK1F81LXl0rMf_!!6000000000441-2-tps-1358-338.png)
+
+## 测试
+
+### 以 table_list_sz 表为例
+
+- 创建，创建成功后，将从“发送邮箱” 发送一条邮件到“接收邮箱”，点击 “CODE” 验证
+
+  ```shell
+  curl --location --request POST 'https://1694024725952210.cn-shenzhen.fc.aliyuncs.com/2016-08-15/proxy/dk-tablestore-mail-demo/info/' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+    "name": "黄小喵",
+    "age": 12,
+    "mail": "12345678@qq.com",
+    "xex": "男"
+  }'
+  ```
+
+  数据返回
+
+  ```js
+  {
+    "data": {
+      "name": "黄小喵",
+      "age": 12,
+      "mail": "12345678@qq.com",
+      "xex": "男"
+    },
+    "message": "数据创建成功"
+  }
+  ```
+  ![iamge](https://img.alicdn.com/imgextra/i3/O1CN01DBfPHM1t4QsR8NTBa_!!6000000005848-0-tps-2786-570.jpg)
+
+
+- 点击 CODE 更新
+![mail](https://img.alicdn.com/imgextra/i2/O1CN01GHFkiE1nJBwbuikdk_!!6000000005068-2-tps-1624-608.png)
+
+ 
+- 验证完成，将修改tablestore数据，以及发送邮件通知
+
+  ![iamge](https://img.alicdn.com/imgextra/i1/O1CN01cK8WMx22Y31qlbchv_!!6000000007131-0-tps-2806-584.jpg)
+
+  ![image](https://img.alicdn.com/imgextra/i1/O1CN01KfKGnK1Yv0PVNIekZ_!!6000000003120-0-tps-1894-636.jpg)
+
+
+> - Serverless Devs 项目：https://www.github.com/serverless-devs/serverless-devs
+> - Serverless Devs 文档：https://www.github.com/serverless-devs/docs
+> - Serverless Devs 钉钉交流群：33947367
---
+++ dk-tablestore-mail/src/functions/mail/index.js
@@ -0,0 +1,71 @@
+const { dk, tablestoreInitialzerPlugin } = require('@serverless-devs/dk');
+const tableStoreEventParse = require('@serverless-devs/tablestore-event-parser');
+
+const nodemailer = require('nodemailer');
+
+const FROM = process.env.mail_from; // 发送邮箱
+const TO = process.env.mail_to; // 接收邮箱
+const PASS = process.env.mail_pass; // 发送邮箱的smtp授权码
+const [, PRODUCT] = process.env.mail_from.split('@'); // 发送平台
+const [SERVIVE] = PRODUCT.split('.');
+
+const TRANSPORT = {
+  // host: 'smtp.ethereal.email',
+  service: SERVIVE, // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+  port: 465, // SMTP 端口
+  secureConnection: true, // 使用了 SSL
+  auth: {
+    user: FROM,
+    pass: PASS, // 这里密码不是qq密码，是你设置的smtp授权码
+  }
+}
+
+// 发送邮件
+const sendMail = async (option) => {
+  const transporter = nodemailer.createTransport(TRANSPORT);
+  const data = await transporter.sendMail(option);
+  console.log('----mail data ----', data);
+};
+
+const handler = dk(async (request) => {
+  const { event = {} } = request;
+  const Records = event.Records || [];
+  if (!Records.length) return;
+  const { Type, PrimaryKey = [], Columns = [] } = Records[0];
+  if (!PrimaryKey.length || !PrimaryKey.length) return;
+  const code = Date.now();
+  const { Value: id } = PrimaryKey.find((item) => item.ColumnName === 'id') || {};
+  const { Value: verified } = Columns.find((item) => item.ColumnName === 'verified') || {};
+
+  switch (Type) {
+    case 'PutRow':
+      const { accountId, region, service } = request.context;
+      const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service.name}`;
+      await sendMail({
+        from: `"验证码" <${FROM}>`,
+        to: TO,
+        subject: 'code',
+        html: `<div>您添加一条数据，请点击<a href="${baseURL}/info/UpdateRow?code=${code}&id=${id}"> CODE </a>验证，添加其他字段</div>`,
+      });
+      break;
+    case 'UpdateRow':
+      if (verified) {
+        await sendMail({
+          from: `"完成" <${FROM}>`,
+          to: TO,
+          subject: '验证完成',
+          text: '通过邮件，修改tablestore，验证完成',
+        });
+      }
+      break;
+
+    default:
+      break;
+  }
+});
+
+handler.use(tablestoreInitialzerPlugin()).use(tableStoreEventParse());
+
+exports.initializer = handler.initializerHandler;
+
+exports.handler = handler;
