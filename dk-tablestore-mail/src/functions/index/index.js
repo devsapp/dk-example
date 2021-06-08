3a8529a41780ee7cd3910331dc6e62f2b90153bc
huangfushan <243973775@qq.com>
2021-06-08 11:56:45 +0800

    feat: 🎸 update yml

--- dk-tablestore-mail/src/functions/index/index.js
+++ dk-tablestore-mail/src/functions/index/index.js
@@ -3,13 +3,13 @@ const fs = require('fs-extra');
 
 const handler = dk({
   'get /index': (request) => {
-    const tablestore_tableName = process.env.tablestore_tableName;
     const html = fs.readFileSync('./index.html', 'utf8');
 
-    // todo 后期可优化
-    let newHtml = html.replace('process.env.tablestore_tableName', tablestore_tableName);
+    //  baseURL，tablestore_tableName
+    const tablestore_tableName = process.env.tablestore_tableName;
     const { accountId, region, service } = request.context;
     const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service.name}`;
+    let newHtml = html.replace('process.env.tablestore_tableName', tablestore_tableName);
     newHtml = html.replace('base_url', baseURL);
 
     return { html: newHtml };
