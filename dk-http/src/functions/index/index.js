f3b68eede5f3e88c8f0c9fb92bb1a41ec25e5e0d
xsahxl <xsahxl@126.com>
2021-06-28 15:51:50 +0800

    feat: 🎸 baseHandler数据格式更改

--- dk-http/src/functions/index/index.js
+++ dk-http/src/functions/index/index.js
@@ -1,12 +1,10 @@
-const { http } = require('@serverless-devs/dk')
+const { dk } = require('@serverless-devs/dk');
 
-const handler = http.onRequest({
-  handler: (request) => {
-    console.log(request)
-    return {
-      body: 'hello world',
-    }
-  },
-})
+const handler = dk((ctx) => {
+  console.log(ctx);
+  return {
+    body: 'hello world',
+  };
+});
 
-exports.handler = handler
+exports.handler = handler;
