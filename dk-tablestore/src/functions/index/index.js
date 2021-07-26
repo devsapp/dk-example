dbbfae370a640244ed79ae343605fd19c38433d4
xsahxl <xsahxl@126.com>
2021-07-26 14:07:20 +0800

    ci: 🎡 new version

--- dk-tablestore/src/functions/index/index.js
+++ dk-tablestore/src/functions/index/index.js
@@ -2,11 +2,11 @@ const { dk } = require('@serverless-devs/dk');
 const fs = require('fs-extra');
 const path = require('path');
 
-const baseHandler = () => {
+const baseHandler = (ctx) => {
   const filePath = path.resolve(__dirname, './index.html');
   const html = fs.readFileSync(filePath, 'utf8');
 
-  const { accountId, region, service } = request.context;
+  const { accountId, region, service } = ctx.context;
   const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service.name}/list`;
   const newHtml = html.replace('base_url', baseURL);
   return { html: newHtml };
