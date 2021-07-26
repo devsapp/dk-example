59e526706066af0a374222e74f044bf3cdb31ab4
xsahxl <xsahxl@126.com>
2021-07-26 09:54:51 +0800

    feat: 🎸 dk-tablestore demo完善

--- dk-tablestore/src/functions/index/index.js
+++ dk-tablestore/src/functions/index/index.js
@@ -4,7 +4,12 @@ const path = require('path');
 
 const baseHandler = () => {
   const filePath = path.resolve(__dirname, './index.html');
-  return { html: fs.readFileSync(filePath, 'utf8') };
+  const html = fs.readFileSync(filePath, 'utf8');
+
+  const { accountId, region, service } = request.context;
+  const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service.name}/list`;
+  const newHtml = html.replace('base_url', baseURL);
+  return { html: newHtml };
 };
 
 const handler = dk(baseHandler);
