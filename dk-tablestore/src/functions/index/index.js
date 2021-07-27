601e98b37c7696131b2693f0052ebc176544b381
xsahxl <xsahxl@126.com>
2021-07-27 22:00:45 +0800

    ci: 🎡 new version

--- dk-tablestore/src/functions/index/index.js
+++ dk-tablestore/src/functions/index/index.js
@@ -6,8 +6,8 @@ const baseHandler = (ctx) => {
   const filePath = path.resolve(__dirname, './index.html');
   const html = fs.readFileSync(filePath, 'utf8');
 
-  const { accountId, region, service } = ctx.context;
-  const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service?.name}/list`;
+  const { accountId, region, service = {} } = ctx.context;
+  const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service.name}/list`;
   const newHtml = html.replace('base_url', baseURL);
   return { html: newHtml };
 };
--- dk-tablestore/src/functions/indexInfo/index.js
+++ dk-tablestore/src/functions/indexInfo/index.js
@@ -6,8 +6,8 @@ const baseHandler = (ctx) => {
   const filePath = path.resolve(__dirname, './index.html');
   const html = fs.readFileSync(filePath, 'utf8');
 
-  const { accountId, region, service } = ctx.context;
-  const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service?.name}/info`;
+  const { accountId, region, service = {} } = ctx.context;
+  const baseURL = `https://${accountId}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${service.name}/info`;
   const newHtml = html.replace('base_url', baseURL);
   return { html: newHtml };
 };
