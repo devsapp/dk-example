93367c3f5e2e39512d0c4200b906e206c07aa9dc
huangfushan <243973775@qq.com>
2021-08-13 18:12:47 +0800

    refactor: 💡 update dk-github

--- dk-github/src/functions/index/index.js
+++ dk-github/src/functions/index/index.js
@@ -1,18 +1,12 @@
-const { dk, githubHandler } = require('@serverless-devs/dk');
+const { github } = require('@serverless-devs/dk');
 const github_path = process.env.github_path;
 const github_secret = process.env.github_secret;
 
-const github = githubHandler({ path: github_path, secret: github_secret });
-
-github.onEvent(data => console.log('监听 event 事件', data.event));
-
-const baseHandler = (ctx) => {
-  const data = github(ctx.req);
-  return {
-    json: data
-  };
-};
-
-const handler = dk(baseHandler);
+const handler = github({
+  handler: (ctx) => {
+    return { json: ctx.req.github }
+  },
+  config: { path: github_path, secret: github_secret }
+});
 
 exports.handler = handler;
