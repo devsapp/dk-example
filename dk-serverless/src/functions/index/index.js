1cb9f3df633e5890f439ac2dd127aeeede8098b6
huangfushan <243973775@qq.com>
2021-09-08 16:02:30 +0800

    feat: 🎸 add dk-serverless

---
+++ dk-serverless/src/functions/index/index.js
@@ -0,0 +1,16 @@
+const { serverless } = require('@serverless-devs/dk');
+const Koa = require('koa');
+const Router = require("koa-router");
+const router = new Router();
+const app = new Koa();
+
+router
+  .get("/koa", (ctx) => {
+    ctx.body = 'Hello Koa!';
+  })
+  .get("/", async (ctx) => {
+    ctx.body = "Hello World!";
+  })
+app.use(router.routes());
+
+exports.handler = serverless(app);
